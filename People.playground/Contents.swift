29712a834efce0a48a98faae2bfc9cc4d5ff3d4b
Maria <dragunova.maria97@gmail.com>
2022-04-11 15:55:06 +0300

    Added Nurse

--- People.playground/Contents.swift
+++ People.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Foundation
 
 class People {
     var name: String
@@ -6,9 +7,9 @@ class People {
     var weight: Double
     var sex: Sex
     
-    enum Sex {
-        case woman
-        case man
+    enum Sex: String {
+        case woman = "woman"
+        case man = "man"
     }
     
     init(name: String, height: Double, weight: Double, sex: Sex) {
@@ -47,7 +48,33 @@ let fighter = Fighter(name: "Kris", height: 1.80, weight: 87, sex: .man)
 
 var arrayOfPeople = [cook, meneger, fighter]
 
+func printDiscription() {
 for person in arrayOfPeople {
-    print("\(person.name), height is \(person.height) m, weight is \(person.weight) kg")
+    print("\(person.name) is \(person.sex.rawValue), height is \(person.height) m, weight is \(person.weight) kg")
     print(person.say())
+}
+}
+
+class Nurse: People {
+    var surname: String
+    var fullName: String {
+        "Nurse, \(name) \(surname)"
+    }
+    
+    init(name: String, surname: String, height: Double, weight: Double, sex: People.Sex) {
+        self.surname = surname
+        super.init(name: name, height: height, weight: weight, sex: sex)
+    }
+}
+
+var nurse = Nurse(name: "Diana", surname: "Smitt", height: 1.55, weight: 45, sex: .woman)
+arrayOfPeople.append(nurse)
+
+for person in arrayOfPeople {
+    if let person = person as? Nurse {
+        print("\(person.fullName) is \(person.sex.rawValue), height is \(person.height) m, weight is \(person.weight) kg")
+        print(person.say())
+    } else {
+        print("\(person.name) is \(person.sex.rawValue), height is \(person.height) m, weight is \(person.weight) kg")
+        print(person.say())}
 }
